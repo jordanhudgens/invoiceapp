33160039b7234af7478e80e7cb80a466ee25336f
mjs1974 <mjs1974@hotmail.co.uk>
2016-11-25 14:54:04 +0000

    add csv uploader

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
+  resources :companies do
+    collection { post :import }
+  end
+  
   devise_for :users
   get 'welcome/index'
-
   get 'welcome/about'
-
   get 'welcome/contact'
-
   get 'welcome/faq'
-
   get 'welcome/pricing'
-
   get 'welcome/features'
 
   resources :invoices
